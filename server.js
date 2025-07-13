34a94bd86c1b1d121e56d263a602b6aa5d176504
phoenix-da-phenom <ehinormichael@ymail.com>
2025-07-13 16:36:34 +0100

    Always make sure uploads directory exist

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs"); // Add fs module
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
@@ -12,8 +13,21 @@ const user = require("./models/user");
 
 const app = express();
 
+// Check if uploads folder and uploads/profile folder exist, create if they don't
+const uploadsDir = path.join(__dirname, "uploads");
+const profileDir = path.join(uploadsDir, "profile");
 
-//Middleware to handle CORS
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir);
+    console.log("Created uploads directory");
+}
+
+if (!fs.existsSync(profileDir)) {
+    fs.mkdirSync(profileDir);
+    console.log("Created uploads/profile directory");
+}
+
+// Middleware to handle CORS
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
@@ -23,27 +37,21 @@ app.use(
     })
 );
 
-
-//Connect Database
+// Connect Database
 connectDB();
 
-// //Middleware
+// Middleware
 app.use(express.json());
 
-
-
 // Serve uploaded files statically
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/add', addRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/add", addRoutes);
 app.use("/api/view", viewRoutes);
 app.use("/api/user", userRoutes);
-// app.use("/api/setting", settingRoutes)
 
-
-
-//Start Server
+// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port!! ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port!! ${PORT}`));
